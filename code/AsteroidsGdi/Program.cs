04458e0056dde3c278b0380b5812b81a637952ef
Fernando Zamora <fernando.zamora.jimenez@gmail.com>
2011-08-12 21:25:04 -0500

    Added comment to program.cs file for test purposes

--- code/AsteroidsGdi/Program.cs
+++ code/AsteroidsGdi/Program.cs
@@ -7,6 +7,7 @@ namespace AsteroidsGdiApp
     {
         /// <summary>
         /// The main entry point for the application.
+        /// Test for git push
         /// </summary>
         [STAThread]
         static void Main()
